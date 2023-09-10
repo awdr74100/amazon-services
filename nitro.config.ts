91d5108c727b38efcbc83aad7fee3c8264da807b
Yi-Ru Lan <a78945612385238@gmail.com>
2023-09-10 16:47:34 +0800

    SES done

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,8 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   runtimeConfig: {
+    AWS_SES_REGION: "...",
+    AWS_SES_SOURCE: "...",
     AWS_S3_BUCKET: "...",
     AWS_S3_REGION: "...",
     AWS_ACCESS_KEY_ID: "...",
--- routes/api/mail.post.ts
+++ routes/api/mail.post.ts
@@ -1,11 +1,55 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
 export default eventHandler(async (event) => {
+  const {
+    AWS_SES_REGION,
+    AWS_SES_SOURCE,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+  } = useRuntimeConfig();
+
+  const sesClient = new SESClient({
+    region: AWS_SES_REGION,
+    credentials: {
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
   try {
-    const body = await readBody<{ title: string; content: string }>(event);
-    console.log(useRuntimeConfig());
-    console.log(body);
+    const body = await readBody<{
+      email: string;
+      title: string;
+      content: string;
+    }>(event);
+
+    const command = new SendEmailCommand({
+      Destination: {
+        ToAddresses: [body.email],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Charset: "UTF-8",
+            Data: body.content,
+          },
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: body.title,
+        },
+      },
+      Source: AWS_SES_SOURCE, // Sandbox mode only allows emails to be sent from verified source
+    });
+
+    const result = await sesClient.send(command);
+
+    console.log(result);
 
     return { success: true };
   } catch (error) {
+    console.log(error);
+
     return { success: false };
   }
 });
